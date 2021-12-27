d9ac56101be0a8056850391dc53f27a464ed6e6c
jahnvisikligar <83291068+jahnvisikligar@users.noreply.github.com>
2021-12-27 00:04:43 +0000

    Add files via upload

---
+++ Assignment 1/Part 1/Regression/Multiple variable/ml_assgn1_2.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+from load_data_ex2 import *
+from normalize_features import *
+from gradient_descent import *
+from calculate_hypothesis import *
+import os
+
+figures_folder = os.path.join(os.getcwd(), 'figures')
+if not os.path.exists(figures_folder):
+    os.makedirs(figures_folder, exist_ok=True)
+
+# This loads our data
+X, y = load_data_ex2()
+
+# Normalize
+X_normalized, mean_vec, std_vec = normalize_features(X)
+
+# After normalizing, we append a column of ones to X, as the bias term
+column_of_ones = np.ones((X_normalized.shape[0], 1))
+# append column to the dimension of columns (i.e., 1)
+X_normalized = np.append(column_of_ones, X_normalized, axis=1)
+
+# initialise trainable parameters theta, set learning rate alpha and number of iterations
+theta = np.zeros((3))
+alpha = 0.0005
+iterations = 100
+
+# plot predictions for every iteration?
+do_plot = True
+
+# call the gradient descent function to obtain the trained parameters theta_final
+theta_final = gradient_descent(X_normalized, y, theta, alpha, iterations, do_plot)
+
+#########################################
+# Write your code here
+# Create two new samples: (1650, 3) and (3000, 4)
+# Calculate the hypothesis for each sample, using the trained parameters theta_final
+# Make sure to apply the same preprocessing that was applied to the training data
+# Print the predicted prices for the two samples
+
+Sample_1 = np.array([[1, 1650, 3], [1, 3000, 4]])
+
+
+N_1 = Sample_1.shape[0]
+
+mean_vector_1 = np.mean(Sample_1, axis=0)
+std_vector_1 = np.std(Sample_1, axis=0)
+
+mean_vector_1 = np.expand_dims(mean_vector_1, axis=0)
+std_vector_1 = np.expand_dims(std_vector_1, axis=0)
+
+repeated_mean_1 = np.repeat(mean_vector_1, N_1, axis=0)
+repeated_std_1 = np.repeat(std_vector_1, N_1, axis=0)
+
+if np.any(repeated_std_1 == 0):
+    repeated_std_1 += np.finfo(float).eps
+
+X_normalized_1 = (Sample_1 - repeated_mean_1) / repeated_std_1
+
+for i in range(0, N_1):
+    hypothesis_1 = calculate_hypothesis(X_normalized_1, theta_final, i)
+    print(i, " : ", hypothesis_1)
+
+########################################
